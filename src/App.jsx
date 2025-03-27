14a542fdacbbf8c42de981e1d847480576821df6
reza hajitabar <rezahajitabarchehre1996@gmail.com>
2025-03-27 05:24:51 -0700

    secend commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import Contact from './components/Contact'
+import Contacts from './components/Contacts'
 function App() {
 
   return (
     <>
      <Header/>
-     <Contact/>
+     <Contacts/>
     </>
   )
 }
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 function Contact() {
+    const [contacts,setContacts]=useState([]);
     const [contact,setContact]=useState({
      name:"",
      family:"",
@@ -15,12 +16,17 @@ function Contact() {
          setContact((contact)=> ({...contact,[name]:value}))
     }
 
+    const clickHandler=()=>{
+        console.log({contact});
+    }
+
   return (
     <div>
       <input type="text" placeholder='name'  value={contact.name} name='name' onChange={changeHandler}/>
       <input type="text" placeholder='family' value={contact.family} name='family' onChange={changeHandler}/>
       <input type="text" placeholder='email' value={contact.email} name='email' onChange={changeHandler}/>
       <input type="text" placeholder='phone' value={contact.phone} name='phone' onChange={changeHandler}/>
+      <button onClick={clickHandler}>Add Contact</button>
     </div>
   )
 }
